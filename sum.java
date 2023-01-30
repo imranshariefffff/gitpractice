af6fe080ce8496f0bb6d8a76dd26d17830007aba
imran sharief <imran@123.com>
2023-01-30 13:36:19 +0530

    committing modified sum.java

--- sum.java
+++ sum.java
@@ -1,10 +1,10 @@
 public class Sum {
-        Run | Debug
-        public static void main(string[] args) {
-            int a = 1;
-            int b = 3;
+    Run | Debug
+    public static void main(string[] args) {
+        int a = 1;
+        int b = 3;
 
-            System.out.println("the sum is " + (a+b));
+        System.out.println(a+b);
     }
 
 }
